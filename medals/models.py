bef7a2952158802100da19ecd590e3f5c2cfd194
fatum <fa-tum@yandex.ru>
2022-01-28 23:05:21 +0300

    Profile migration

--- medals/models.py
+++ medals/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.db.models.signals import pre_delete
 from smart_selects.db_fields import ChainedForeignKey
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 class Country(models.Model):
     name = models.CharField(max_length = 200)
@@ -172,3 +174,15 @@ def deleter(sender, **kwargs):
     pass
 
 pre_delete.connect(deleter, sender=Medal)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
